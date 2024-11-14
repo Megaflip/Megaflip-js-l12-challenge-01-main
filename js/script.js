0dd57f7998653350a64738849d703b67cff78383
Megan McConnell <megaflip@gmail.com>
2024-11-13 21:00:29 -0500

    Final commit

--- js/script.js
+++ js/script.js
@@ -10,11 +10,22 @@ const getImage = async function () {
   selectRandomImage(images);
 };
 
-const selectRandomImage = function(images) {
+const selectRandomImage = function (images) {
     const randomIndex = Math.floor(Math.random() * images.length);
     //console.log(randomIndex);
     const randomImage = images[randomIndex];
     //console.log(randomImage);
+    displayImage(randomImage);
 };
 
+const displayImage = function(randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+};
+
+button.addEventListener("click", function () {
 getImage();
+});
